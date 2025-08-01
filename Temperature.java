a928945c703ba84da9c96ca2f2cccbba3e24a9de
rehana938 <mantinoonerehana934@gmail.com>
2025-08-01 15:27:49 +0530

    Add files via upload

---
+++ Temperature.java
@@ -0,0 +1,26 @@
+
+public class Temperature {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int temp=50;
+		if(temp>45)
+		{
+			System.out.println("ram should stay home..");
+		}
+		else if(temp>=30 &&temp>=45)
+		{
+			System.out.println("he should carry water");
+		}
+		else if(temp<=0)
+		{
+			System.out.println(" he can go normallly.");
+		}
+		else
+		{
+		 System.out.println("Decide whether ram should ge out and what precautions he sholud take");
+		}
+
+	}
+
+}
