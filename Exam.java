9b3d7133d340c6c0f4975af65028ce078d0e96ac
rehana938 <mantinoonerehana934@gmail.com>
2025-08-01 15:27:07 +0530

    Add files via upload

---
+++ Exam.java
@@ -0,0 +1,35 @@
+
+public class Exam {
+
+	public static void main(String[] args) {
+	
+		int avg=90;
+		 if(avg>=35)
+		{
+			System.out.println("she passed if all subject");
+		}
+		else if(avg>=90)
+		{
+			System.out.println( "she gets outstanding rank");
+		}
+		else if(avg>=75)
+		{
+			System.out.println("She gets distinction");
+		}
+		else if(avg>=60)
+		{
+			System.out.println("she gets first class");
+		}
+		else if(avg<=35)
+		{
+			System.out.println("pass");
+		}
+		else {
+			System.out.println("fails");
+		}
+
+		
+
+	}
+
+}
